f5d773c556feb349273c9519340efb86bf85a12b
08Miguel24 <MichaelAveuc571@gmail.com>
2023-01-07 19:52:37 +0800

    update

--- extract_highlights_from_pdf.py
+++ extract_highlights_from_pdf.py
@@ -2,7 +2,7 @@ import fitz
 
 
 
-doc = fitz.open("../../Research Papers/~Domain-Specific Knowledge Graph Construction for Semantic Analysis.pdf")
+doc = fitz.open("../../../Research Papers/~Domain-Specific Knowledge Graph Construction for Semantic Analysis.pdf")
 # Total page in the pdf
 print(len(doc))
 # taking page for further processing
@@ -11,7 +11,7 @@ page = doc[0]
 # list to store the co-ordinates of all highlights
 highlights = []
 # loop till we have highlight annotation in the page
-annot = page.firstAnnot
+annot = page.first_annot
 while annot:
     if annot.type[0] == 8:
         all_coordinates = annot.vertices
@@ -26,13 +26,15 @@ while annot:
     annot = annot.next
 
 
-all_words = page.getTextWords()
+all_words = page.get_text_words()
 
 
 # List to store all the highlighted texts
 highlight_text = []
 for h in highlights:
     sentence = [w[4] for w in all_words if fitz.Rect(w[0:4]).intersect(h)]
-    highlight_text.append(" ".join(sentence))
+    appended_words = " ".join(sentence)
+    highlight_text.append(appended_words)
+    print(appended_words)
 
-print(" ".join(highlight_text))
+
